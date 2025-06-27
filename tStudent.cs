8cd9c22590e91fd1cc0d4ffb13839fd8ebec2d85
Hajime <chen2346897@gmail.com>
2025-06-27 11:39:49 +0800

    加入專案檔案。

---
+++ tStudent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyModel_DBFirst.Models;
+
+//3.1 打開tStudent.cs檔案
+//3.2 撰寫在View上顯示的欄位內容(需using System.ComponentModel.DataAnnotations)
+
+public partial class tStudent
+{
+    [Display(Name ="學號")]
+    public string fStuId { get; set; } = null!;
+
+    [Display(Name = "姓名")]
+    public string fName { get; set; } = null!;
+
+    [Display(Name = "電子郵件")]
+    public string? fEmail { get; set; }
+
+    [Display(Name = "成績")]
+    public int? fScore { get; set; }
+}
